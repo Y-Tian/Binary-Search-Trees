eacf8c003de3b76f2ac411a73d17690f53978c18
tonytiancompsci <t.tian4321@gmail.com>
2017-06-19 21:05:40 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binary.search.trees;
+
+/**
+ *
+ * @author tony.tian
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        BinarySearchTrees bst = new BinarySearchTrees();
+        bst.add('d');
+        bst.add('s');
+        bst.add('f');
+        bst.add('a');
+        bst.add('a');
+        bst.add('b');
+        bst.add('c');
+        bst.add('d');
+        bst.main();
+        //bst.remove(15);
+        //System.out.println();
+        //bst.main();
+    }
+}
